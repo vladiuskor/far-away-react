1aebf3c4174eb43997265aef35bdea2a3530e5a1
Vlad Korobko <max.vlad.kor@gmail.com>
2024-02-28 18:40:16 +0200

    Added list rendering for items

--- src/App.js
+++ src/App.js
@@ -1,35 +1,61 @@
+const initialItems = [
+    {id: 1, description: "Passports", quantity: 2, packed: false},
+    {id: 2, description: "Socks", quantity: 12, packed: false},
+    {id: 3, description: "Charger", quantity: 1, packed: true},
+];
+
+
 function App() {
-    return(
+    return (
         <div className="app">
-            <Logo />
-            <Forms />
-            <PackingList />
-            <Stats />
+            <Logo/>
+            <Forms/>
+            <PackingList/>
+            <Stats/>
         </div>
     )
 }
+
 function Logo() {
-    return(
+    return (
         <h1>🌴Far Away 🧳</h1>
     )
 }
+
 function Forms() {
-    return(
+    return (
         <div className="add-form">
             <h3>What do you need for your 😍 trip?</h3>
         </div>
     )
 }
+
 function PackingList() {
-    return(
-        <div className="list">LIST</div>
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map(item => <Item item={item}/>)}
+            </ul>
+        </div>
     )
 }
+
 function Stats() {
-    return(
+    return (
         <footer className="stats">
-            🧳You have X items on your list, and you already packed X (X%)
+            🧳 You have X items on your list, and you already packed X (X%)
         </footer>
+    )
+}
+
+function Item({item}) {
+    return (
+        <li>
+            <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
     )
 }
 
