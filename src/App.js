c9e5e1d437bb93ef5224dcdd7952df9dd474ab02
Vlad Korobko <max.vlad.kor@gmail.com>
2024-03-04 16:45:32 +0200

    Added controlled elements to Form section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialItems = [
     {id: 1, description: "Passports", quantity: 2, packed: false},
     {id: 2, description: "Socks", quantity: 12, packed: false},
@@ -23,10 +25,43 @@ function Logo() {
 }
 
 function Forms() {
+    const [description, setDescription] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if(!description) return;
+
+        const newItem = {
+            description,
+            quantity,
+            packed: false,
+            id: Date.now()
+        }
+        console.log(newItem);
+
+        setDescription('');
+        setQuantity(1);
+    }
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select
+                value={quantity}
+                onChange={e => setQuantity(Number(e.target.value))}
+            >
+                {Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+            </select>
+            <input
+                type="text"
+                placeholder="Item..."
+                onChange={(e) => setDescription(e.target.value) }
+                value={description}
+            />
+            <button>Add</button>
+        </form>
     )
 }
 
@@ -34,7 +69,7 @@ function PackingList() {
     return (
         <div className="list">
             <ul>
-                {initialItems.map(item => <Item item={item}/>)}
+                {initialItems.map(item => <Item item={item} key={item.id}/>)}
             </ul>
         </div>
     )
